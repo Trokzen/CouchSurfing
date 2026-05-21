7e2572cfbadcf18d702224cafbbfdba2b1f9020a
Robert <Nadzar@yandex.ru>
2026-05-21 16:45:00 +0300

    Delete app directory

--- app/routers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Routers package - API endpoints modules
-"""
-from app.routers.auth import router as auth_router
-from app.routers.listing import router as listing_router
-from app.routers.booking import router as booking_router
-from app.routers.review import router as review_router
-
-__all__ = ["auth_router", "listing_router", "booking_router", "review_router"]
--- app/services/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Services __init__ - Экспорт сервисов
-"""
-from app.services.auth import AuthService
-
-__all__ = ["AuthService"]
